2c42fdb8170e993a1895cb819b68dfba24b09925
mbstevenson96 <mbstevenson96@gmail.com>
2023-02-28 16:44:26 -0500

    update root elements

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -4,6 +4,13 @@ const ExpenseItem = () => {
   return ( 
     <>
       <h2>ExpenseItem Component</h2>
+      <div>
+        Date
+      </div>
+      <div>
+        <h2>Title</h2>
+        <div>Amount</div>
+      </div>
     </>
   );
 }
