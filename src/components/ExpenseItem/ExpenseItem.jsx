2cc6587307586c7145fc5e865e27e33712fbc49e
mbstevenson96 <mbstevenson96@gmail.com>
2023-03-21 12:52:18 -0400

    update class names in styling to match module css

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -2,14 +2,13 @@ import styles from './ExpenseItem.module.css'
 
 const ExpenseItem = () => {
   return ( 
-    <>
-      <h2>ExpenseItem Component</h2>
+    <div className={styles.expenseItem}>
       <div>March 28, 2021</div>
       <div>
         <h2>Car Insurance</h2>
         <div>$294.67</div>
       </div>
-    </>
+    </div>
   );
 }
 
